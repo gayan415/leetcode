05687a6ea4a67b4f519b422d120c6b8770510a04
gayanj <gayan@bluedrop.com>
2020-01-04 13:29:07 -0700

    Structure the repo with Algo and DB's questions

---
+++ database/0175_Combine_Two_Tables.sql
@@ -0,0 +1,11 @@
+"""
+https://leetcode.com/problems/combine-two-tables/
+"""
+
+SELECT
+	Person.FirstName,
+	Person.LastName,
+	Address.City,
+	Address.State
+FROM Person
+LEFT JOIN Address ON Address.PersonId = Person.PersonId
